073ee11620c75d2fdfebb54870435107d4bb0ffc
Jakub Cernohuby <ofcaas@seznam.cz>
2011-02-22 19:50:00 +0100

    add test_get_schema, add global

--- tests/drivers/driverTesting.php
+++ tests/drivers/driverTesting.php
@@ -6,7 +6,6 @@
 */
 require_once 'SetTesting.php'; //setting of test
 require_once 'function.php'; //functions required to testing
-require_once '../../adminer/include/adminer.inc.php';
 require_once '../../adminer/include/functions.inc.php';
 
 
@@ -18,6 +17,7 @@ class driverTesting extends PHPUnit_Framework_TestCase
     protected $_c = null;
     protected $array_minDb = array();
     protected $driver = "";
+    protected $d_name = "";
     public $connection = "";
     /*
     * Function before start test
@@ -25,7 +25,7 @@ class driverTesting extends PHPUnit_Framework_TestCase
     public function setUp() 
     {
         global $driver, $adminer, $connection;
-        global $server, $user, $pass, $make_db;
+        global $server, $user, $pass, $make_db, $d_name;
 
         $driver = $_GET['driver_test'];
         
@@ -34,20 +34,9 @@ class driverTesting extends PHPUnit_Framework_TestCase
         $_GET[$d_name] = "driver"; //@todo what is setting to $_GET
         require_once DRIVER_FOLDER . $driver; //to add testing file
         
-        /* set up for connect()
-        define("SERVER", $server[$d_name]);
-        $_GET["username"] = $user[$d_name];
-        define("DRIVER", $d_name);
-        $_SESSION["pwds"][DRIVER][SERVER][$_GET["username"]] = $pass[$d_name];
-        */
-        
         $connection = new Min_DB;
         $connection->connect($server[$d_name], $user[$d_name], $pass[$d_name]);
-        
-        //$connection->select_db('mytest');
         $connection->query($make_db[$d_name]);
-        
-        //$connection = connect();
     }
 
     /*
@@ -55,9 +44,9 @@ class driverTesting extends PHPUnit_Framework_TestCase
     */    
     public function tearDown()
     {
-        global $driver, $connection, $make_db_name;
+        global $driver, $connection, $make_db_name, $d_name;
         
-        $db[] = $make_db_name[getNameOfDriver($driver)];
+        $db[] = $make_db_name[$d_name];
         drop_databases($db);
         unset($this->_c);
     }
@@ -66,32 +55,13 @@ class driverTesting extends PHPUnit_Framework_TestCase
     * Tests of Min_DB
     */    
     
-    
-    
-    
-    // quote string
-    public function test_quote()
-    {
-        global $driver, $connection;
-        
-        switch (getNameOfDriver($driver)) {
-            case "mssql":
-                $this->assertEquals($connection->quote("test'test"), "'test''test'");
-                break;
-            default:
-                break;
-        }
-
-    }
-    
     // connect to server
 		public function test_connect()
     {
         global $driver, $connection;
-        global $server, $user, $pass, $make_db;
-        $d_name = getNameOfDriver($driver);
+        global $server, $user, $pass, $make_db, $d_name;
         
-        switch (getNameOfDriver($driver)) {
+        switch ($d_name) {
             case "mssql":
                 $this->assertFalse($connection->connect("", "", "")); //test of connect with false
                 $connection->connect($server[$d_name], $user[$d_name], $pass[$d_name]); //return to normal connect
@@ -102,12 +72,30 @@ class driverTesting extends PHPUnit_Framework_TestCase
 
     }
     
+    
+    // quote string
+    public function test_quote()
+    {
+        global $driver, $connection, $d_name;
+        
+        switch ($d_name) {
+            case "mssql":
+                $this->assertEquals($connection->quote("test'test"), "'test''test'");
+                break;
+            default:
+                break;
+        }
+
+    }
+    
+    
+    
     // send query to server
     public function test_query()
     {
-        global $driver, $connection;
+        global $driver, $connection, $d_name;
         
-        switch (getNameOfDriver($driver)) {
+        switch ($d_name) {
             case "mssql":
                 //$this->assertFalse($connection->query("randomtext"));
                 break;
@@ -124,9 +112,9 @@ class driverTesting extends PHPUnit_Framework_TestCase
     // escape string 
     public function test_idf_escape()
     {
-        global $driver;
+        global $driver, $d_name;
         
-        switch (getNameOfDriver($driver)) {
+        switch ($d_name) {
             case "mssql":
                 $this->assertEquals(idf_escape("test]test"), "[test]]test]");
                 break;
@@ -143,9 +131,9 @@ class driverTesting extends PHPUnit_Framework_TestCase
     // escape string and add scheme
     public function test_table()
     {
-        global $driver;
+        global $driver, $d_name;
 
-        switch (getNameOfDriver($driver)) {
+        switch ($d_name) {
             case "mssql":
                 $_GET["ns"] = "owner";
                 $this->assertEquals(table("test]test"), "[owner].[test]]test]");
@@ -165,7 +153,7 @@ class driverTesting extends PHPUnit_Framework_TestCase
     //@todo problem with query function
     public function test_get_databases()
     {
-        global $driver, $connection;
+        global $driver, $connection, $d_name;
 
         $this->assertType(PHPUnit_Framework_Constraint_IsType::TYPE_ARRAY, get_databases());
     }
@@ -173,9 +161,9 @@ class driverTesting extends PHPUnit_Framework_TestCase
     // show how formulate SQL with limit
     public function test_limit()
     {
-        global $driver;
+        global $driver, $d_name, $d_name;
                                                  
-        switch (getNameOfDriver($driver)) {
+        switch ($d_name) {
             case "mssql":
                 $tmp = " TOP (5) querywhere";
                 $this->assertEquals(limit("query", "where", "5"), $tmp);
@@ -193,9 +181,9 @@ class driverTesting extends PHPUnit_Framework_TestCase
     // show how formulate SQL with limit with 1
     public function test_limit1()
     {
-        global $driver;
+        global $driver, $d_name;
 
-       switch (getNameOfDriver($driver)) {
+       switch ($d_name) {
             case "mssql":
                 $tmp = " TOP (1) querywhere";
                 $this->assertEquals(limit1("query", "where"), $tmp);
@@ -210,20 +198,13 @@ class driverTesting extends PHPUnit_Framework_TestCase
         } 
     }
     
-     //@todo problem with query
-    public function test_db_collation()
-    {
-        global $driver;
-
         
-    }
-    
     
     public function test_engines()
     {
-        global $driver;
+        global $driver, $d_name;
 
-       switch (getNameOfDriver($driver)) {
+       switch ($d_name) {
             case "mssql":
                 $tmp = engines();
                 $this->assertTrue(empty($tmp));
@@ -240,9 +221,9 @@ class driverTesting extends PHPUnit_Framework_TestCase
     
     public function test_is_view()
     {
-        global $driver;
+        global $driver, $d_name;
 
-       switch (getNameOfDriver($driver)) {
+       switch ($d_name) {
             case "mssql":
                 $tmp["Engine"] = "VIEW";
                 $this->assertTrue(is_view($tmp));
@@ -259,25 +240,25 @@ class driverTesting extends PHPUnit_Framework_TestCase
 
     function test_fk_support()
     {
-        global $driver;
+        global $driver, $d_name;
 
-        if(getNameOfDriver($driver)=="mysql"){
-
-           $this->assertEquals(fk_support("status"), true);
-        }
-        if(getNameOfDriver($driver)=="mssql"){
-           $this->assertEquals(fk_support("status"), true);
-        }
-    	if(getNameOfDriver($driver)=="oracle"){
-           $this->assertEquals(fk_support("status"), true);
-        }
+        switch ($d_name) {
+            case "mssql":
+                $this->assertEquals(fk_support("status"), true);
+                break;
+            case "mysql":
+                //@todo
+                break;
+            default:
+                break;
+        } 
     }
     
     public function test_information_schema()
     {
-        global $driver;
+        global $driver, $d_name;
 
-       switch (getNameOfDriver($driver)) {
+       switch ($d_name) {
             case "mssql":
                 $this->assertFalse(information_schema(""));
                 break;
@@ -292,9 +273,9 @@ class driverTesting extends PHPUnit_Framework_TestCase
     
     public function test_exact_value()
     {
-        global $driver;
+        global $driver, $d_name;
 
-        switch (getNameOfDriver($driver)) {
+        switch ($d_name) {
             case "mssql":
                 $this->assertEquals(exact_value("test'test"), "'test''test'");
                 break;
@@ -308,9 +289,9 @@ class driverTesting extends PHPUnit_Framework_TestCase
     
     public function test_create_database()
     {
-        global $driver, $connection;
+        global $driver, $connection, $d_name;
 
-        switch (getNameOfDriver($driver)) {
+        switch ($d_name) {
             case "mssql":
                 $connection->query("CREATE DATABASE testTest2");
                 $tmp = create_database("testTest", "Czech_BIN");
@@ -326,9 +307,9 @@ class driverTesting extends PHPUnit_Framework_TestCase
     
     public function test_rename_database()
     {
-        global $driver, $connection;
+        global $driver, $connection, $d_name;
 
-        switch (getNameOfDriver($driver)) {
+        switch ($d_name) {
             case "mssql":
                 define("DB", "testTest");
                 $tmp = rename_database("testTest2", "Czech_CI_AS");
@@ -341,9 +322,9 @@ class driverTesting extends PHPUnit_Framework_TestCase
     
     public function test_drop_databases()
     {
-        global $driver, $connection;
+        global $driver, $connection, $d_name;
 
-        switch (getNameOfDriver($driver)) {
+        switch ($d_name) {
             case "mssql":
                 $db[] = "testTest";
                 $db[] = "testTest2";
@@ -360,9 +341,9 @@ class driverTesting extends PHPUnit_Framework_TestCase
     
     public function test_auto_increment()
     {
-        global $driver;
+        global $driver, $d_name;
 
-       switch (getNameOfDriver($driver)) {
+       switch ($d_name) {
             case "mssql":
                 $tmp = " IDENTITY(5,1) PRIMARY KEY";
                 $_POST["Auto_increment"] = "5"; 
@@ -380,9 +361,9 @@ class driverTesting extends PHPUnit_Framework_TestCase
     
     public function test_trigger_options()
     {
-        global $driver;
+        global $driver, $d_name;
 
-       switch (getNameOfDriver($driver)) {
+       switch ($d_name) {
             case "mssql":
                 $tmp = array(
 			                   "Timing" => array("AFTER", "INSTEAD OF"),
@@ -399,14 +380,14 @@ class driverTesting extends PHPUnit_Framework_TestCase
         } 
     }
 
-    /*
+    
      public function test_get_schema()
     {
-        global $driver;
+        global $driver, $d_name;
 
-       switch (getNameOfDriver($driver)) {
+       switch ($d_name) {
             case "mssql":
-                $this->assertTrue(get_schema());
+                $this->assertEquals(get_schema(), "dbo");
                 break;
             case "mysql":
                 //@todo
@@ -416,13 +397,13 @@ class driverTesting extends PHPUnit_Framework_TestCase
                 break;
         } 
     }
-    */
+    
     
     public function test_set_schema()
     {
-        global $driver;
+        global $driver, $d_name;
 
-       switch (getNameOfDriver($driver)) {
+       switch ($d_name) {
             case "mssql":
                 $this->assertTrue(set_schema(""));
                 break;
@@ -437,9 +418,9 @@ class driverTesting extends PHPUnit_Framework_TestCase
     
     public function test_show_variables()
     {
-        global $driver;
+        global $driver, $d_name;
 
-       switch (getNameOfDriver($driver)) {
+       switch ($d_name) {
             case "mssql":
                 $tmp = show_variables();
                 $this->assertTrue(empty($tmp));
@@ -455,9 +436,9 @@ class driverTesting extends PHPUnit_Framework_TestCase
 
     public function test_show_status()
     {
-        global $driver;
+        global $driver, $d_name;
 
-       switch (getNameOfDriver($driver)) {
+       switch ($d_name) {
             case "mssql":
                 $tmp = show_status();
                 $this->assertTrue(empty($tmp));
@@ -474,9 +455,9 @@ class driverTesting extends PHPUnit_Framework_TestCase
     
     public function test_support()
     {
-        global $driver;
+        global $driver, $d_name;
 
-       switch (getNameOfDriver($driver)) {
+       switch ($d_name) {
             case "mssql":
                 $this->assertEquals(support("trigger"), 1);
                 $this->assertEquals(support("view"), 1);
